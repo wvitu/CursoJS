178b66601e85bf307651bfa14291323a9cc43fa3
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-03-13 21:27:18 -0300

    Exercício de código de sistema de multas em condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 25
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // condição simples
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Paraguai'
+console.log(`Nascido em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
