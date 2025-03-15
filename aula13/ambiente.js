33feef314394563e3916dd2dc6d091c109648ae1
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-03-14 22:20:42 -0300

    Teste de do e while

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1 
+while (c <= 5) {
+console.log(`Passo ${c}`)
+c++
+}
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
